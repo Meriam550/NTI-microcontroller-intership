4a915738b0795c78637b3c691435e4a61021dfca
Meriam550 <meriamgoda@gmail.com>
2026-09-03 21:24:16 +0300

    Add files via upload

---
+++ Labs/day4_lab1_LCD/day4_lab1_LCD/LCD.h
@@ -0,0 +1,25 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+#include "std.h"
+
+
+//init
+void LCD_init(void);
+
+//char
+void LCD_sendChar(u8 Data);
+
+//send command
+void LCD_SendCommand(u8 command);
+
+//Send String
+void LCD_SendString (u8 *str);
+ 
+ void GoToXY(u8 x,u8 y);
+ 
+ void LCD_voidWriteNumber(s32 num);
+ 
+ 
+ 
+ #endif
