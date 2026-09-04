2b592bc63f3247f6e9b9792663f47be84b63942a
Meriam550 <meriamgoda@gmail.com>
2026-09-04 21:00:19 +0300

    Add files via upload

---
+++ Labs/day4_lab2_keypad/day4_lab2_keypad/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+DIO_prog.c
+
+keypad.c
+
+day4_lab2_keypad.c
+
+SevSegment.c
+
---
+++ Labs/day4_lab2_keypad/day4_lab2_keypad/keypad.c
@@ -0,0 +1,64 @@
+#include "std.h"
+#include "bitmath.h"
+#include "DIO_int.h"
+#include "keypad.h"
+#include <util/delay.h>
+
+void keypad_init(void) {
+	
+	DIO_voidSetPinDirection(DPORTA, PIN0, INPUT);
+	DIO_voidSetPinDirection(DPORTA, PIN1, INPUT);
+	DIO_voidSetPinDirection(DPORTA, PIN2, INPUT);
+	DIO_voidSetPinDirection(DPORTA, PIN3, INPUT);
+	
+	
+	DIO_voidSetPinDirection(DPORTB, PIN0, OUTPUT);
+	DIO_voidSetPinDirection(DPORTB, PIN1, OUTPUT);
+	DIO_voidSetPinDirection(DPORTB, PIN2, OUTPUT);
+	DIO_voidSetPinDirection(DPORTB, PIN3, OUTPUT);
+	
+	
+	DIO_voidSetPinValue(DPORTA, PIN0, HIGH);
+	DIO_voidSetPinValue(DPORTA, PIN1, HIGH);
+	DIO_voidSetPinValue(DPORTA, PIN2, HIGH);
+	DIO_voidSetPinValue(DPORTA, PIN3, HIGH);
+	
+	
+	DIO_voidSetPinValue(DPORTB, PIN0, HIGH);
+	DIO_voidSetPinValue(DPORTB, PIN1, HIGH);
+	DIO_voidSetPinValue(DPORTB, PIN2, HIGH);
+	DIO_voidSetPinValue(DPORTB, PIN3, HIGH);
+}
+
+u8 GetPressedKey(void) {
+	
+	u8 key = NO_KEY;
+	u8 row, col;
+	
+	u8 arr[4][4] = {
+{  '/',         '9',         '8',         '7'  }, 
+{  '*',         '6',         '5',         '4'  },
+{  '-',         '3',         '2',         '1'  },
+{  '+',         '=',         '0',         'C'  }
+	};
+	
+	for (col = 0; col < 4; col++) {
+		
+		DIO_voidSetPortValue(DPORTB, ~(1 << col));
+		
+		for (row = 0; row < 4; row++) {
+			if (DIO_u8ReadPinValue(DPORTA, row) == LOW) {
+				_delay_ms(20);  
+				if (DIO_u8ReadPinValue(DPORTA, row) == LOW) {
+				
+					while (DIO_u8ReadPinValue(DPORTA, row) == LOW);
+					DIO_voidSetPortValue(DPORTB, 0xFF);  
+					return arr[row][col];
+				}
+			}
+		}
+	}
+	
+	DIO_voidSetPortValue(DPORTB, 0xFF);  
+	return NO_KEY;  
+}
