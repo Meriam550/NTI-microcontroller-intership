ba8b97fe332e057200670259de4ab0f8a49ee9fa
Meriam_Aldarder <meriamgoda@gmail.com>
2026-09-09 15:57:36 +0300

    Add files via upload

---
+++ Application/day8_lab3.c
@@ -0,0 +1,32 @@
+/*
+ * day8_lab3.c
+ *
+ * Created: 9/8/2026 10:16:27 PM
+ * Author : Meriam
+ */ 
+
+#define F_CPU 1000000UL
+#include "SERVO.h"
+#include <util/delay.h>
+
+int main(void)
+{
+	SERVO_Init();
+	
+	while(1)
+	{
+		for(u8 angle = 180; angle <= 180; angle += 10)
+		{
+			SERVO_SetAngle(angle);
+			_delay_ms(500);
+		}
+		
+		for(u8 angle = 180; angle >= 0; angle -= 10)
+		{
+			SERVO_SetAngle(angle);
+			_delay_ms(500);
+		}
+	}
+	
+	return 0;
+}
