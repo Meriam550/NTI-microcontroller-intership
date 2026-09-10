aae001b081eb208f320e04c69cb3f75ebf3c1f47
Meriam_Aldarder <meriamgoda@gmail.com>
2026-09-10 13:10:11 +0300

    Add files via upload

---
+++ Application/day9_lab1(R).c
@@ -0,0 +1,57 @@
+/*
+ * day9_lab1(R).c
+ *
+ * Created: 9/9/2026 8:00:26 PM
+ * Author : Meriam
+ */ 
+
+#include "std.h"
+#include "USART.h"
+#include "DIO_int.h"
+#include "LED_interface.h"
+
+// LED Configuration
+LED_Type Led1 = {DPORTC, PIN0, ACTIVE_HIGH};
+LED_Type Led2 = {DPORTC, PIN1, ACTIVE_HIGH};
+LED_Type Led3 = {DPORTC, PIN2, ACTIVE_HIGH};
+LED_Type Led4 = {DPORTC, PIN3, ACTIVE_HIGH};
+
+int main(void)
+{
+	// Initialize LEDs
+	LED_voidInit(&Led1);
+	LED_voidInit(&Led2);
+	LED_voidInit(&Led3);
+	LED_voidInit(&Led4);
+	
+	// Initialize USART at 9600 baud
+	USART_Init(9600);
+	
+	while(1)
+	{
+		// Wait for data from transmitter
+		u8 received_data = USART_Get_Char();
+		
+		// Toggle corresponding LED based on received number
+		switch(received_data)
+		{
+			case 1:  // Changed from '1' to 1
+			LED_voidToggle(&Led1);
+			break;
+			case 2:  // Changed from '2' to 2
+			LED_voidToggle(&Led2);
+			break;
+			case 3:  // Changed from '3' to 3
+			LED_voidToggle(&Led3);
+			break;
+			case 4:  // Changed from '4' to 4
+			LED_voidToggle(&Led4);
+			break;
+			default:
+			// Ignore invalid data
+			break;
+		}
+	}
+	
+	
+}
---
+++ Application/day9_lab1(T).c
@@ -0,0 +1,59 @@
+/*
+ * day9_lab1(T).c
+ *
+ * Created: 9/9/2026 7:56:11 PM
+ * Author : Meriam
+ */ 
+
+#include "std.h"
+#include "USART.h"
+#include "DIO_int.h"
+
+// Button connected to PD0 for sending data
+#define SEND_BUTTON_PIN    PIN0
+#define SEND_BUTTON_PORT   DPORTD
+
+int main(void)
+{
+	// Set PD0 as input with internal pull-up enabled
+	DIO_voidSetPinDirection(SEND_BUTTON_PORT, SEND_BUTTON_PIN, INPUT);
+	DIO_voidSetPinValue(SEND_BUTTON_PORT, SEND_BUTTON_PIN, HIGH);  // Enable internal pull-up
+	// Initialize button as input with pull-up
+	DIO_voidSetPinDirection(SEND_BUTTON_PORT, SEND_BUTTON_PIN, INPUT);
+	DIO_voidSetPinValue(SEND_BUTTON_PORT, SEND_BUTTON_PIN, HIGH);  // Enable pull-up
+	
+	// Initialize USART at 9600 baud
+	USART_Init(9600);
+	
+	u8 counter = 1;
+	
+	while(1)
+	{
+		// Check if button is pressed (active low with pull-up)
+		if(DIO_u8ReadPinValue(SEND_BUTTON_PORT, SEND_BUTTON_PIN) == LOW)
+		{
+			// Simple debounce delay
+			u16 i;
+			for(i = 0; i < 5000; i++);
+			
+			// Confirm button still pressed
+			if(DIO_u8ReadPinValue(SEND_BUTTON_PORT, SEND_BUTTON_PIN) == LOW)
+			{
+				// Send number (1-4, then cycle back)
+				USART_SendChar(counter);
+				
+				// Increment counter (1, 2, 3, 4, 1, 2, ...)
+				counter++;
+				if(counter > 4)
+				{
+					counter = 1;
+				}
+				
+				// Wait for button release
+				while(DIO_u8ReadPinValue(SEND_BUTTON_PORT, SEND_BUTTON_PIN) == LOW);
+			}
+		}
+	}
+	
+	
+}
