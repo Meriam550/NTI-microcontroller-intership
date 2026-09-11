0c5cfbab61c7da616720b3238466ba6ec46fa9a2
Meriam_Aldarder <meriamgoda@gmail.com>
2026-09-12 00:33:38 +0300

    Add files via upload

---
+++ MCAL/ADC/ADC.h
@@ -0,0 +1,23 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "../../serv/std.h"
+
+#define ADMUX_Reg   *((volatile u8*)0x27)
+#define ADCSRA_Reg  *((volatile u8*)0x26)
+#define ADCH_Reg    *((volatile u8*)0x25)
+#define ADCL_Reg    *((volatile u8*)0x24)
+
+#define ADC0 0
+#define ADC1 1
+#define ADC2 2
+#define ADC3 3
+#define ADC4 4
+#define ADC5 5
+#define ADC6 6
+#define ADC7 7
+
+void ADC_Init(void);
+u16 ADC_Read(u8 channel);
+
+#endif
